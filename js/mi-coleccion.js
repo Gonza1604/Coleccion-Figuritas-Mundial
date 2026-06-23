03aa004aae220114d45523aed19258233d9ceaef
Valen Romano <valentinaromanop@gmail.com>
2026-06-23 18:33:09 -0300

    Agregue todos los objetos al array figuritas

--- js/mi-coleccion.js
+++ js/mi-coleccion.js
@@ -8,11 +8,91 @@ const figuritas=[
     año: 2026,
     pais: "Croacia"},
     {
-     tipo: "",
-    imagen: "../iconos-e-imagenes/imagenes-figus/",
-    alt: "Figura de ",
-    claseImg: "figura-",
+     tipo: "Común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-messi.jpg",
+    alt: "Figura de Messi",
+    claseImg: "figura-messi",
     año: 2026,
-    pais: ""
+    pais: "Argentina"
+    },
+     {
+     tipo: "Común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-neymar.jpg",
+    alt: "Figura de Neymar",
+    claseImg: "figura-neymar",
+    año: 2026,
+    pais: "Brasil"
+    },
+     {
+     tipo: "Rara",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-neymar",
+    alt: "Figura de Ronaldo",
+    claseImg: "figura-Ronaldo",
+    año: 2026,
+    pais: "Portugal"
+    },
+     {
+     tipo: "Común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-lamine.jpg",
+    alt: "Figura de Lamine Yamal",
+    claseImg: "figura-lamine",
+    año: 2026,
+    pais: "España"
+    },
+     {
+     tipo: "Colaboracion con coca cola",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-harrykane.jpg",
+    alt: "Figura de Harry Kane",
+    claseImg: "figura-harrykane",
+    año: 2026,
+    pais: "Inglaterra"
+    },
+     {
+     tipo: "común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-enzoFernandez.jpg",
+    alt: "Figura de Enzo Fernandez",
+    claseImg: "figura-enzofernandez",
+    año: 2026,
+    pais: "Argentina"
+    },
+     {
+     tipo: "común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-julianAlvarez.jpg",
+    alt: "Figura de Julian Alvarez",
+    claseImg: "figura-julianalvarez",
+    año: 2026,
+    pais: "Argentina"
+    },
+     {
+     tipo: "común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-haaland.jpg",
+    alt: "Figura de Haaland",
+    claseImg: "figura-Haaland",
+    año: 2026,
+    pais: "Noruega"
+    },
+     {
+     tipo: "dorada",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-messiDorada.jpg",
+    alt: "Figura de Messi",
+    claseImg: "figura-messiDorada",
+    año: 2026,
+    pais: "Argentina"
+    },
+     {
+     tipo: "Colaboracion con coca cola",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-lautaroM.jpg",
+    alt: "Figura de Lautaro Martinez",
+    claseImg: "figura-lautaromartinez",
+    año: 2026,
+    pais: "Argentina"
+    },
+     {
+     tipo: "dorada",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-mbappe.jpg",
+    alt: "Figura de Kylian Mbappé",
+    claseImg: "figura-mbappe",
+    año: 2026,
+    pais: "Francia"
     }
-]
+ ]
