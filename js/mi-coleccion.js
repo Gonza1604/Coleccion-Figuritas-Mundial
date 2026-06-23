4f4bbd753f2f813aebf3a92f21db8940b1a137b0
Valen Romano <valentinaromanop@gmail.com>
2026-06-23 16:25:03 -0300

    agrego cambio en js

---
+++ js/mi-coleccion.js
@@ -0,0 +1,18 @@
+const figuritas=[
+{
+ id: 1,
+    tipo: "Común",
+    imagen: "../iconos-e-imagenes/imagenes-figus/figu-modric.jpg",
+    alt: "Figura de Modric",
+    claseImg: "figura-modric",
+    año: 2026,
+    pais: "Croacia"},
+    {
+     tipo: "",
+    imagen: "../iconos-e-imagenes/imagenes-figus/",
+    alt: "Figura de ",
+    claseImg: "figura-",
+    año: 2026,
+    pais: ""
+    }
+]
