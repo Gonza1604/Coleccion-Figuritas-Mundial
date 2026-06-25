9d5e25951450c55363870eafd81b9b7e000541e8
jeremias <arcejereluisguillermo@gmail.com>
2026-06-25 19:18:45 -0300

    agrego login y registro

---
+++ Coleccion-Figuritas-Mundial-main/js/iniciar-sesion.js
@@ -0,0 +1,18 @@
+document.querySelector(".login-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("usuario").value;
+  const contraseña = document.getElementById("password").value;
+
+  const datos = localStorage.getItem("usuarios");
+  const usuarios = datos ? JSON.parse(datos) : [];
+
+  const encontrado = usuarios.find(u => u.nombre === nombre && u.contraseña === contraseña);
+
+  if (encontrado) {
+    sessionStorage.setItem("usuarioLogueado", nombre);
+    window.location.href = "../index.html";
+  } else {
+    alert("Usuario o contraseña incorrectos.");
+  }
+});
